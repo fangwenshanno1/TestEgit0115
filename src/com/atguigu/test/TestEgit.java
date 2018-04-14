571cb76e7e94337388202939328efa1a0323da67
fangwenshanno1 <37733414+fangwenshanno1@users.noreply.github.com>
2018-04-14 16:29:21 +0800

    edit by remote

--- src/com/atguigu/test/TestEgit.java
+++ src/com/atguigu/test/TestEgit.java
@@ -3,7 +3,7 @@ package com.atguigu.test;
 public class TestEgit {
 	
 	public static void main(String[] args) {
-		System.out.println("hello world!");
+		System.out.println("hello world!!!!!!");
 	}
 
 }
